d0853204aba05228e387736efbb8b74d8dc0b1a5
Eric van der Toorn <E.A.vanderToorn@student.tudelft.nl>
2021-12-21 22:10:17 +0100

    clean up day 17

--- src/years/year2021/day17.rs
+++ src/years/year2021/day17.rs
@@ -14,45 +14,6 @@ struct Day17 {
     y_bounds: (isize, isize),
 }
 impl Day {
-    fn simulate_other(&self, x_speed: isize, y_speed: isize) {
-        // y(t) = (a-t) t + y(t-1)
-        // y(t) = 1/6 (3a(t^2+t+2) - t(t+1)(2t+1))
-        // x(t) = (b - max(b, t)) t + x(t-1)
-        // x(t) = b + 1/6 ((3b-2t-1)t(t+1)-(3b-2b-1)b(b+1))
-        // we can reverse the formulae to get the other speed
-        // given a particular y_initial (a), we can get the x that would allow for an intersection
-        // then we can see if that would happen at a full step or not
-        let t = self.count_ysteps_till_intersection(10).unwrap() as isize;
-        // let t = t as f64;
-        // let b = (-18 * t
-        //     ^ 3 - 27 * t
-        //     ^ 2 + (3 as f64).sqrt()
-        //         * sqrt(-729 * t ^ 4 - 1458 * t ^ 3 - 2493 * t ^ 2 - 1764 * t - 1372)
-        //         - 9 * t)
-        //     ^ (1 / 3) / (2 ^ (1 / 3) * 3 ^ (2 / 3))
-        //         - ((2 / 3) ^ (1 / 3) * (-9 * t ^ 2 - 9 * t - 21))
-        //             / (3 * (-18 * t
-        //                 ^ 3 - 27 * t
-        //                 ^ 2 + sqrt(3)
-        //                     * sqrt(-729 * t ^ 4 - 1458 * t ^ 3 - 2493 * t ^ 2 - 1764 * t - 1372)
-        //                     - 9 * t)
-        //                 ^ (1 / 3));
-        todo!()
-    }
-
-    fn count_ysteps_till_intersection(&self, y_speed: isize) -> Option<usize> {
-        let mut y = 0;
-        let mut steps = 0;
-        while y < self.y_bounds.1 {
-            y += y_speed;
-            steps += 1;
-            if y >= self.y_bounds.0 && y <= self.y_bounds.1 {
-                return Some(steps);
-            }
-        }
-        return None;
-    }
-
     fn simulate(&self, x_speed: isize, y_speed: isize) -> Option<isize> {
         // simulates the movement of the sub till it passes the square
         let mut cur_x_speed = x_speed;
@@ -60,11 +21,9 @@ impl Day {
         let mut x = 0;
         let mut y = 0;
         let mut y_max = 0;
-        // let mut step = 1;
         loop {
             x += cur_x_speed;
             y += cur_y_speed;
-            // println!("at step {}: x={}, y={}", step, x, y);
             y_max = std::cmp::max(y_max, y);
 
             if x >= self.x_bounds.0
@@ -72,11 +31,8 @@ impl Day {
                 && y <= self.y_bounds.0
                 && y >= self.y_bounds.1
             {
-                // println!("Found square at step {}", step);
                 return Some(y_max);
             } else if x > self.x_bounds.1 || y < self.y_bounds.1 {
-                // println!("Stepped out of bounds at step {}", step);
-                // println!("{:?}", (x, y));
                 return None;
             }
             // x speed is slowed till it reaches zero
@@ -87,7 +43,6 @@ impl Day {
             }
             // y speed keeps decreasing
             cur_y_speed -= 1;
-            // step += 1;
         }
     }
 }
@@ -97,7 +52,6 @@ impl AOCDay for Day {
     type Output = isize;
 
     fn part_one(&mut self) -> Self::Output {
-        println!("{:?}", self);
         // we know that our y  value should always be positive
         // we want to maximize the y value that we can have
         // we can do this by simulating the movement of the sub
@@ -106,15 +60,11 @@ impl AOCDay for Day {
         let mut max_y = 0;
         for y_speed in 1..=self.x_bounds.0 {
             for x_speed in 1..=(self.x_bounds.0 / 2) {
-                // let (x_speed, y_speed) = (7, 2);
-                println!("Trying {} {}", x_speed, y_speed);
                 let result = self.simulate(x_speed, y_speed);
                 if let Some(y) = result {
                     max_y = std::cmp::max(max_y, y);
                 }
-                // break
             }
-            // break
         }
         max_y
     }
@@ -122,19 +72,12 @@ impl AOCDay for Day {
         let mut velos = vec![];
         for y_speed in (self.y_bounds.1)..=self.x_bounds.1 {
             for x_speed in 1..=(self.x_bounds.1) {
-                // let (x_speed, y_speed) = (7, 2);
-                // println!("Trying {} {}", x_speed, y_speed);
                 let result = self.simulate(x_speed, y_speed);
                 if let Some(_) = result {
-                    // println!("success");
                     velos.push((x_speed, y_speed));
                 }
-                // break
             }
-            // break
         }
-        // velos.sort_unstable();
-        // println!("{:?}", velos);
         velos.len() as isize
     }
 }
